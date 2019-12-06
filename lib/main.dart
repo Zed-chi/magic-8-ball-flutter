d41387d552fdafa82125f7086109a9505459c5f0
zed <zed@mail.ru>
2019-12-07 00:45:48 +0300

    added two widgets with random changing of an image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(home: BallPage(),),
+);
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ball"),
       ),
+      body: Ball(),
     );
+  }
+}
+
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+
+class _BallState extends State<Ball> {
+  int num = 1;
+  
+  void changeNumber(){
+    setState(()=>{
+      num = Random().nextInt(5)+1
+    });
+    print("clicked");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+            onPressed: changeNumber,
+            padding: EdgeInsets.all(0),
+            child: Image(
+              
+              image: AssetImage("images/ball$num.png"),
+            ),
+    ));
+  }
+}
